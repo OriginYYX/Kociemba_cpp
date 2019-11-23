4818329b9bfd196730c5c4b85bd0647dadc3c505
OriginYYX <oringin@icloud.com>
2019-11-23 19:53:45 +0800

    Kociemba算法基类
    可以接受一个魔方状态的输入，将其转换为一个由若干个整数描述的状态

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include "cube.hpp"
+int a;
+int main()
+{
+    cube cube;
+    cube.input();
+    std::cout<<"okkkkkkkkkkkk"<<std::endl;
+    cube.output();
+
+    cube.int_cube_input();
+    cube.int_cube_output();
+
+    return 0;
+}
