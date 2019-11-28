6e4e7fef3ea91f5f5161d0a4de751d5c2dd304ea
OriginYYX <oringin@icloud.com>
2019-11-28 17:43:02 +0800

    char2int

--- int_move.cpp
+++ int_move.cpp
@@ -1,5 +1,19 @@
 #include "int_move.hpp"
 using namespace std;
+int char2int(char num){
+    if(num<='9'){
+        return num-48;
+    }
+    else{
+        return (int)num - 87;
+    }
+}
+int int_swap(char now_pos1,char now_pos2,char now_pos3,char now_pos4,char tar_pos1,char tar_pos2,char tar_pos3,char tar_pos4){
+    int temp=0;
+    for (int i=0;i<4;i++){
+
+    }
+}
 int int_cube::int_cube_output(){
     for(int i=0;i<2;i++){
     for(int j=0;j<20;j++){
