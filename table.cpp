0a9b640e9b1c4b0f644b35b6d51f6c227c27ae5e
OriginYYX <oringin@icloud.com>
2019-12-28 02:35:27 +0800

    new

--- table.cpp
+++ table.cpp
@@ -1,7 +1,5 @@
 #include "table.hpp"
 
 void table::make_CornerOrientation_MoveTable(){
-    for (int i=0;i<6560;i++){
-        
-    }
+
 }
--- table.hpp
+++ table.hpp
@@ -1,12 +1,8 @@
 #ifndef TABLE_HPP_INCLUDED
 #define TABLE_HPP_INCLUDED
 #include "int_move.hpp"
-#include <fstream>
-#include <iostream>
-extern int CornerOrientation_MoveTable[6560][12];
 class table{
     void make_CornerOrientation_MoveTable();
-    void show_CornerOrientation_MoveTable();
 };
 
 #endif // TABLE_HPP_INCLUDED
